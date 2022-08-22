ccd3a444069925098a6eb3488663fea3e526d5e5
Professor Reddig <reddign@etown.edu>
2022-08-22 15:06:50 -0400

    Adjusting the main page.

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
-Welcome to the main page of the EMCS database
+Welcome to the main page of the EMCS database project.
+
+This project will be worked on by multiple students in CS 341 - Software Engineering.
 

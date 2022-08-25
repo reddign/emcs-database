8d40b67ddf60d9b70cd144709f99102a8cafe04f
August Bednar <bednara@etown.edu>
2022-08-25 10:00:35 -0400

    Updated Bednar Contributor Page

--- src/contributors/august_bednar.php
+++ src/contributors/august_bednar.php
@@ -11,6 +11,6 @@ require("../includes/header.php");
   </div>
 
   August is a junior data science major at Etown.
-  He is currently working with team Delta to provide access to student survey information.
+  
 <?PHP
 require("../includes/footer.php");

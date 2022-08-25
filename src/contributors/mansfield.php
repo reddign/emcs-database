0024c4f272655cdba0a69fdb64f199cac481a5c8
Nate <mansfieldn@etown.edu>
2022-08-25 10:08:21 -0400

    pushed contributor page

---
+++ src/contributors/mansfield.php
@@ -0,0 +1,17 @@
+<?PHP
+$path="../";
+require("../includes/header.php");
+
+?>
+ <!-- Header -->
+  <div class="w3-container" style="margin-top:80px" id="showcase">
+    <h1 class="w3-jumbo"><b>Contributor</b></h1>
+    <h1 class="w3-xxxlarge w3-text-red"><b>Nate Mansfield</b></h1>
+    <hr style="width:50px;border:5px solid red" class="w3-round">
+  </div>
+
+  <!--Nate is a senior computer science major at Etown.
+  He is currently working with team Delta to implement features for the site.-->
+
+<?PHP
+require("../includes/footer.php");

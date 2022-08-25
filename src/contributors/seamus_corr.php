e3232d3203df6249dee199f9ad903f02bceb1084
Seamus Corr <corrs@etown.edu>
2022-08-25 09:44:30 -0400

    Added my personal page

---
+++ src/contributors/seamus_corr.php
@@ -0,0 +1,17 @@
+<?PHP
+$path="../";
+require("../includes/header.php");
+
+?>
+ <!-- Header -->
+  <div class="w3-container" style="margin-top:80px" id="showcase">
+    <h1 class="w3-jumbo"><b>Contributor</b></h1>
+    <h1 class="w3-xxxlarge w3-text-red"><b>Seamus Corr</b></h1>
+    <hr style="width:50px;border:5px solid red" class="w3-round">
+  </div>
+
+  Seamus is a senior Computer Science major at Etown.
+  he is currently working with team Epsilon to implement some features for the site.
+
+<?PHP
+require("../includes/footer.php");

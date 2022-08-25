5ad46d6c4f2da0cef8046603a69e2ec83d89b93c
Kelsey Knappenberger <kelseyknappenberger@gmail.com>
2022-08-25 09:36:13 -0400

    adding my page to group project

---
+++ src/contributors/kelsey_knappenberger.php
@@ -0,0 +1,17 @@
+<?PHP
+$path="../";
+require("../includes/header.php");
+
+?>
+ <!-- Header -->
+  <div class="w3-container" style="margin-top:80px" id="showcase">
+    <h1 class="w3-jumbo"><b>Contributor</b></h1>
+    <h1 class="w3-xxxlarge w3-text-red"><b>Jane Doe</b></h1>
+    <hr style="width:50px;border:5px solid red" class="w3-round">
+  </div>
+
+  Kelsey is a senior Information Systems major at Etown.
+  She is currently working with team Omega to create a log-in page for the site.
+
+<?PHP
+require("../includes/footer.php");

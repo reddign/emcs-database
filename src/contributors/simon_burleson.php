b281d3de90171e6f37c8f1660472029a74a10283
simonb2527 <global>
2022-08-25 10:29:18 -0400

    adding simon contributor page

---
+++ src/contributors/simon_burleson.php
@@ -0,0 +1,16 @@
+<?PHP
+$path="../";
+require("../includes/header.php");
+
+?>
+ <!-- Header -->
+  <div class="w3-container" style="margin-top:80px" id="showcase">
+    <h1 class="w3-jumbo"><b>Contributor</b></h1>
+    <h1 class="w3-xxxlarge w3-text-red"><b>Simon Burleson</b></h1>
+    <hr style="width:50px;border:5px solid red" class="w3-round">
+  </div>
+
+  Simon is a Junior Computer Science major.
+  He is currently working on  
+<?PHP
+require("../includes/footer.php");

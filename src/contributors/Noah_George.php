a789d157c7888e13ecb5b711cec04fa2bc0dd03a
NoahWGeorge <georgen1@etown.edu>
2022-08-30 10:56:48 -0400

    adding to my project

---
+++ src/contributors/Noah_George.php
@@ -0,0 +1,16 @@
+<?PHP
+$path="../";
+require("../includes/header.php");
+
+?>
+ <!-- Header -->
+  <div class="w3-container" style="margin-top:80px" id="showcase">
+    <h1 class="w3-jumbo"><b>Contributor</b></h1>
+    <h1 class="w3-xxxlarge w3-text-red"><b>Professor Reddig</b></h1>
+    <hr style="width:50px;border:5px solid red" class="w3-round">
+  </div>
+
+  My name is Noah George and I am a seinor computer scienece major.
+  
+<?PHP
+require("../includes/footer.php");

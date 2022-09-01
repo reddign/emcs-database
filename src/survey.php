1222b4559f50cdc024d36cf54833352b51d8e1e7
August Bednar <bednara@etown.edu>
2022-09-01 09:40:26 -0400

    removed bad text

--- src/survey.php
+++ src/survey.php
@@ -15,7 +15,7 @@ require("includes/header.php");
   ?>
   <p>
     <label for="interest_txt">What are your interests:</label>
-    <textarea rows = "3"><input type="text" name="interests" id="interest_txt"></textarea>
+    <textarea rows = "3"></textarea>
   </p>
 
 

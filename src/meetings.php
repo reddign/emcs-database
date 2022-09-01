3e962d68a73367495d7eb5e74d3ec2afec15f74a
8ethan <weitkampe@etown.edu>
2022-09-01 10:15:18 -0400

    Added more to the meetings page

--- src/meetings.php
+++ src/meetings.php
@@ -4,7 +4,8 @@ require("includes/header.php");
 require("../config.php");
 require("functions/basic_html_functions.php");
 require("functions/database_functions.php");
-# require("functions/student_form_functions.php");
+#require("functions/student_form_functions.php");
+require("functions/meeting_form_functions.php");
 
 ?>
  <!-- Header -->
@@ -12,15 +13,15 @@ require("functions/database_functions.php");
     display_page_heading("Meetings","Log & Notes");
   ?>
   <?php
-  /*
+  $page = isset($_GET["page"])?$_GET["page"]:"search";
   switch($page){
     case "search":
       $string = isset($_GET["search"])?$_GET["search"]:"";
-      $students = get_student_by_name($string);
-      display_search_form();
-      display_student_list($students);
+      #$students = get_student_by_name($string);
+      display_search_meeting_form();
+      display_meeting_list($meetings);
       break;
-    case "add":
+    /*case "add":
       display_student_form();
       break;
     case "edit":
@@ -29,14 +30,13 @@ require("functions/database_functions.php");
       display_student_list($student);
       display_student_form($student);
       break;
-    case "student":
+    case "meeting":
       $sid = isset($_GET["sid"])?$_GET["sid"]:"";
       $student = get_student($sid);
       display_student_info($student);
-      break;
-
+      break;*/
   }
-  */
+  
   require("includes/footer.php");
   ?>
 

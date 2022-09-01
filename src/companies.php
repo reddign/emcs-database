dbfef2f06768596325f0d271ee4cab60e2cd99a6
Joe <kuttehj@etown.edu>
2022-09-01 09:12:10 -0400

    merged changes

--- src/companies.php
+++ src/companies.php
@@ -3,10 +3,38 @@ $path = '';
 require("../config.php");
 require("functions/basic_html_functions.php");
 require("functions/database_functions.php");
-require("functions/company_form_functions.php");
+require("functions/companies_form_functions.php");
 $page = isset($_GET["page"])?$_GET["page"]:"search";
 require("includes/header.php");
 
+display_page_heading("Students","");
+
+display_student_page_navigation("Students");
+
+
+switch($page){
+  case "search":
+    $string = isset($_GET["search"])?$_GET["search"]:"";
+    $students = get_student_by_name($string);
+    display_search_form();
+    display_student_list($students);
+    break;
+  case "add":
+    display_student_form();
+    break;
+  case "edit":
+    $sid = isset($_GET["sid"])?$_GET["sid"]:"";
+    $student = get_student($sid);
+    display_student_list($student);
+    display_student_form($student);
+    break;
+  case "student":
+    $sid = isset($_GET["sid"])?$_GET["sid"]:"";
+    $student = get_student($sid);
+    display_student_info($student);
+    break;
+
+}
 ?>
  <!-- Header -->
   <div class="w3-container" style="margin-top:80px" id="showcase">

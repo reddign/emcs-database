a64dc685881c9d0c6d9df719985555088d6a25ef
Professor Reddig <reddign@etown.edu>
2022-09-06 13:18:29 -0400

    Reduced the top margin in the generic page heading function.

--- src/functions/basic_html_functions.php
+++ src/functions/basic_html_functions.php
@@ -2,7 +2,7 @@
 
 function display_page_heading($mainHeading = "Change Main Heading",$subHeading){
     echo '<!-- Header -->';
-    echo '<div class="w3-container" style="margin-top:80px" id="showcase">';
+    echo '<div class="w3-container" style="margin-top:40px" id="showcase">';
     echo '<h1 class="w3-jumbo"><b>'.$mainHeading.'</h1>';
     if($subHeading!=""){
         echo '<h1 class="w3-xxxlarge w3-text-red"><b>'.$subHeading.'.</b></h1>';

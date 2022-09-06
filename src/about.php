800365468979f618ac9271cab78dc2acf1898c4a
Professor Reddig <reddign@etown.edu>
2022-09-06 13:18:04 -0400

    Use the newer page heading function.

--- src/about.php
+++ src/about.php
@@ -1,15 +1,9 @@
 ﻿<?PHP
 $path = '';
+require("functions/basic_html_functions.php");
 require("includes/header.php");
-
+display_page_heading("About","");
 ?>
- <!-- Header -->
-  <div class="w3-container" style="margin-top:80px" id="showcase">
-    <h1 class="w3-jumbo"><b>About</b></h1>
-    <h1 class="w3-xxxlarge w3-text-red"><b>EMCS Database.</b></h1>
-    <hr style="width:50px;border:5px solid red" class="w3-round">
-  </div>
-
   The EMCS database was a project from the CS341 Software Engineering during the Fall of 2022. 
   During the first 4 weeks of class students worked as part of the EC🗲DZ software team using a Git repository to share code.  The students used their programming skills to add functionality to the software and practiced working as a agile software team.  Below is a picture of the students in the class and a detailed list of contributors.
  

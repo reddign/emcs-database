a309f7be22e40dc8ea81714591b4904a54f36db8
K1ngMatt <suttonm@etown.edu>
2022-09-06 20:48:21 -0400

    Changed alex_fox.php

--- src/contributors/alex_fox.php
+++ src/contributors/alex_fox.php
@@ -7,12 +7,11 @@ require("../includes/header.php");
   <div class="w3-container" style="margin-top:80px" id="showcase">
     <h1 class="w3-jumbo"><b>Contributor</b></h1>
 <<<<<<<< HEAD:src/contributors/Nathan_Griffin.php
-    <h1 class="w3-xxxlarge w3-text-red"><b>Nathan Griffin</b></h1>
+    <h1 class="w3-xxxlarge w3-text-red"><b>Alex Fox</b></h1>
     <hr style="width:50px;border:5px solid red" class="w3-round">
   </div>
 
-  Nathan is a senior engineering major at Etown.
-  He is currently working with team Xi to implement mobile-friendly features for the site.
+   Alex Fox is a computer science major at Etown.
 ========
     <h1 class="w3-xxxlarge w3-text-red"><b>Alexander Fox</b></h1>
     <hr style="width:50px;border:5px solid red" class="w3-round">

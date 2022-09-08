86308f5dd422871e348225c0cff7bd45da739713
August Bednar <bednara@etown.edu>
2022-09-08 09:32:08 -0400

    removed functions for test

--- src/survey.php
+++ src/survey.php
@@ -1,7 +1,7 @@
 <?PHP
 $path = '';
 require("includes/header.php");
-require("functions/survey_functions.php");
+//require("functions/survey_functions.php");
 
 
 display_survey_page_navigation("Survey");

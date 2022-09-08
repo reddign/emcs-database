ce8d868e5ac3bbe572155e500f9f31c5548623bc
Professor Reddig <reddign@etown.edu>
2022-09-08 08:56:36 -0400

    Shorten site name to improve look on mobile device.

--- src/index.php
+++ src/index.php
@@ -5,7 +5,7 @@ require("includes/header.php");
 ?>
  <!-- Header -->
   <div class="w3-container" style="margin-top:80px" id="showcase">
-    <h1 class="w3-jumbo"><b>EMCS Career Link</b></h1>
+    <h1 class="w3-jumbo"><b>EMCS Career</b></h1>
     <h1 class="w3-xxxlarge w3-text-red"><b>Database.</b></h1>
     <hr style="width:50px;border:5px solid red" class="w3-round">
   </div>

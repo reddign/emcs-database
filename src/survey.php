97c7c92a1cc3a46465fc85ac784689136e3cb89a
August Bednar <bednara@etown.edu>
2022-09-08 09:48:07 -0400

    fied header placement

--- src/survey.php
+++ src/survey.php
@@ -4,7 +4,16 @@ require("includes/header.php");
 require("functions/basic_html_functions.php");
 require("functions/database_functions.php");
 require("functions/survey_functions.php");
+?>
 
+ <!-- Header -->
+ <div class="w3-container" style="margin-top:80px" id="showcase">
+    <h1 class="w3-jumbo"><b>Survey</b></h1>
+    <h1 class="w3-xxxlarge w3-text-red"><b>Student Interest.</b></h1>
+    <hr style="width:50px;border:5px solid red" class="w3-round">
+  </div>
+
+<?php
 $page = isset($_GET["page"])?$_GET["page"]:"add";
 display_survey_page_navigation("Survey");
 
@@ -17,13 +26,8 @@ switch($page){
       break;
   }
   
-  ?>
- <!-- Header -->
-  <div class="w3-container" style="margin-top:80px" id="showcase">
-    <h1 class="w3-jumbo"><b>Survey</b></h1>
-    <h1 class="w3-xxxlarge w3-text-red"><b>Student Interest.</b></h1>
-    <hr style="width:50px;border:5px solid red" class="w3-round">
-  </div>
+  
 
-<?php
+
+
 require("includes/footer.php");

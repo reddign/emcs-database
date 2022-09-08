c8cea8b620aba08df1b0a78ef08c7cdae4f72a9f
Joe <kuttehj@etown.edu>
2022-09-08 08:46:37 -0400

    updated display_search_form

--- src/functions/companies_form_function.php
+++ src/functions/companies_form_function.php
@@ -39,8 +39,8 @@ function display_company_page_navigation($currentPage){
     echo $navHTML;
 }
 function display_search_form(){
-    echo '<h2>Search for a student by Name</h2><form method=get action="students.php">
-        Enter Student Name:<input name="search" type="text">
+    echo '<h2>Search for a company by Name</h2><form method=get action="companies.php">
+        Enter Company Name:<input name="search" type="text">
         <input name="page" type="hidden" value="search">
         <input type="submit" value="Search">
     </form><br/><br/>';

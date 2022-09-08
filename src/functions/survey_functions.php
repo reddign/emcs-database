82d5b63014841334502e33420ab525ccde93637f
August Bednar <bednara@etown.edu>
2022-09-08 08:42:54 -0400

    added addSurvey and display_survey_page_nav func

--- src/functions/survey_functions.php
+++ src/functions/survey_functions.php
@@ -13,5 +13,27 @@ mail($to,$subject,$txt);
 function code_checker() {
     
 }
+function addSurvey($arrayData){
+    $surveyID = $arrayData["surveyID"];
+    $interests = $arrayData["interests"];
+    $careerGoals = $arrayData["careerGoals"];
+    $studentID = $arrayData["studentID"];
+    $pdo = connect_to_db();
+    $stmt = $pdo->prepare("insert into student (firstName,lastName,gradYear,alumni) VALUES (:first,:last,:gradYr,:alum)");
+    $stmt->execute([':first' => $first_name, ":last"=> $last_name, ":gradYr"=>$gradYear,":alum"=>$alumni]);
+    $sid = $pdo->lastInsertId();
+    header("location:students.php?page=student&sid=".$sid."&message=Student Added");
+  
+}
+
+function display_survey_page_navigation($currentPage){
+    $navHTML  = '<h4><div style="margin-top:5px;margin-bottom:45px;">';
+    $navHTML .= '<a href="students.php?page=search" class="selected">Email Code</a>';
+    $navHTML .= ' | ';
+    $navHTML .= '<a href="students.php?page=add">Take Survey</a>';
+    $navHTML .= ' <div> </h4>';
+    
+    echo $navHTML;
+}
 
 ?>

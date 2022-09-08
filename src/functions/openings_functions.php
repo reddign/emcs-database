2568f62a76f8b7baa1fb44bb58fb0882923c3563
knappenbergerk <knappenbergerk@etown.edu>
2022-09-08 16:13:09 -0400

    Added the search function

--- src/functions/openings_functions.php
+++ src/functions/openings_functions.php
@@ -19,3 +19,41 @@ function display_search_openings(){
     </form><br/><br/>';
 
 }
+function get_job_by_name($word){
+    if($word==""){
+        return get_all_openings_from_db();
+    }
+    $pdo = connect_to_db();
+    $stmt = $pdo->prepare("SELECT * FROM job_opportunity WHERE title like :title");
+    $stmt->execute([':title' => "%".$word."%"]);
+    $data = [];
+    while($job =  $stmt->fetch(PDO::FETCH_ASSOC)){
+        $data[] = $job;
+    } 
+    
+    return $data;
+}    
+function get_all_openings_from_db(){
+    $pdo = connect_to_db();
+    $data = $pdo->query("SELECT * FROM job_opportunity order by title")->fetchAll();
+    return $data;
+}
+function display_search_form(){
+    echo '<h2>Search For A Job Opening By Title:</h2><form method=get action="openings.php">
+        Enter Job Opening: <input name="search" type="text">
+        <input job="page" type="hidden" value="search">
+        <input type="submit" value="Search">
+    </form><br/><br/>';
+
+}
+function display_job_list($data=null){
+    if(!is_array($data)){
+        echo "";
+        return;
+    }
+    foreach ($data as $row) {
+            echo "<a href='openings.php?page=opening&jid=".$row['jobID']."'>";
+            echo $row['title']."<br />\n";
+            echo "</a>";
+    }
+}
--- src/openings.php
+++ src/openings.php
@@ -24,4 +24,30 @@ require("includes/header.php");
 display_page_heading("Job Openings","");
 display_openings_navigation("Openings");
 
+switch($page){
+  case "search":
+    $string = isset($_GET["search"])?$_GET["search"]:"";
+    $openings = get_job_by_name($string);
+    display_search_form();
+    display_job_list($openings);
+    break;
+  /*
+  case "add":
+    display_student_form();
+    break;
+  case "edit":
+    $sid = isset($_GET["sid"])?$_GET["sid"]:"";
+    $student = get_student($sid);
+    display_student_form($student);
+    break;
+    
+  case "student":
+    $sid = isset($_GET["sid"])?$_GET["sid"]:"";
+    $student = get_student($sid);
+    display_student_info($student);
+    break;
+  */
+
+}
+
 require("includes/footer.php");

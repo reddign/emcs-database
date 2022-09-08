7c03cbca6cea6db1afb7edf0257575b0403c7f73
Adam Omundsen <17weebly@gmail.com>
2022-09-08 08:45:34 -0400

    changed personal file

--- src/contributors/adam_omundsen.php
+++ src/contributors/adam_omundsen.php
@@ -12,7 +12,7 @@ require("../includes/header.php");
 
   Adam is a junior computer science major at Etown.
   He is currently working with team Beta to provide access to industry contacts data.
-  He is also a robot who likes to work at the chocolate factory.
+  He is also a robot who likes
 
 <?PHP
 require("../includes/footer.php");

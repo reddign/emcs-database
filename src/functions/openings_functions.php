38c23b50b7ee25a45071ce8bf5e6ec9d1cc4252c
knappenbergerk <knappenbergerk@etown.edu>
2022-09-08 11:42:47 -0400

    Trying something out

--- src/functions/openings_functions.php
+++ src/functions/openings_functions.php
@@ -15,7 +15,7 @@ function display_openings_navigation($currentPage){
     
     echo $navHTML;
 }
-function display_search_form(){
+function display_search_openings(){
     echo '<h2>Search for an opening</h2><form method=get action="students.php">
         Enter Opening Name:<input name="search" type="text">
         <input name="page" type="hidden" value="search">

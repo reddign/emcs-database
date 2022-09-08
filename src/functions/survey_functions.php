27134b64063fabe04b4587a6f89d2a6fe49ab050
K1ngMatt <suttonm@etown.edu>
2022-09-08 08:24:31 -0400

    survey_functions

--- src/functions/survey_functions.php
+++ src/functions/survey_functions.php
@@ -1,3 +1,17 @@
-<?php
- 
+<?PHP
+
+
+function email_survey_send($student_email) {
+$to = $student_email;
+$subject = "Student Interest Survey";
+$txt = "Dear Blah, you have not completed your student interest survey. Please follow the link *link*
+and complete the survey. Thank you, Yo motha";
+
+mail($to,$subject,$txt);
+}
+
+function code_checker() {
+    
+}
+
 ?>

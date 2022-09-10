5f73d68f2a92dbe09931ef63a27212eb1142784b
Joe <kuttehj@etown.edu>
2022-09-09 20:57:44 -0400

    test

--- src/contributors/joe_kutteh.php
+++ src/contributors/joe_kutteh.php
@@ -11,6 +11,7 @@ require("../includes/header.php");
   </div>
 
   Joe is a senior engineering major at Etown. This is an edit of his page.
+  This is a test.
   
 <?PHP
 require("../includes/footer.php");

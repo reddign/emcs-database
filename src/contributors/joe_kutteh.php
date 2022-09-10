a0e4297097716e9797b394a50dca643e84c81d76
Joe <kuttehj@etown.edu>
2022-09-09 21:44:12 -0400

    revised

--- src/contributors/joe_kutteh.php
+++ src/contributors/joe_kutteh.php
@@ -10,13 +10,7 @@ require("../includes/header.php");
     <hr style="width:50px;border:5px solid red" class="w3-round">
   </div>
 
-<<<<<<< HEAD
-  Joe is a senior engineering major at Etown. This is an edit of his page. 
-  test 1
-=======
   Joe is a senior engineering major at Etown. This is an edit of his page.
-  
->>>>>>> bcf5a96 (Adding)
   
 <?PHP
 require("../includes/footer.php");

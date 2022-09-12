663ef0c56ec8b5b9f78d68668f54beb433b191d0
Professor Reddig <reddign@etown.edu>
2022-09-12 13:37:02 -0400

    Experiment with changes to the font.

--- src/about.php
+++ src/about.php
@@ -2,38 +2,40 @@
 $path = '';
 require("functions/basic_html_functions.php");
 require("includes/header.php");
-display_page_heading("About","");
+display_small_page_heading("About","");
 ?>
   The EMCS database was a project from the CS341 Software Engineering during the Fall of 2022. 
-  During the first 4 weeks of class students worked as part of the EC🗲DZ software team using a Git repository to share code.  The students used their programming skills to add functionality to the software and practiced working as a agile software team.  Below is a picture of the students in the class and a detailed list of contributors.
+  During the first 4 weeks of class students worked as part of the EC🗲DZ software team using a Git repository to share code.  The students used their programming skills to add functionality to the software and practiced working as an agile software team.  Below is a picture of the students in the class and a detailed list of contributors.
  
   <img src="images/classPhoto.png" style="width:800px;margin-top:30px;margin-bottom:30px;">
  
  
   <BR/>
-  Derek Baldwin<BR/> 
-  <a href="contributors/august_bednar.php">August Bednar</a><BR/>
-  <a href="contributors/simon_burleson.php">Simon Burleson</a><BR/>
-  <a href="contributors/seamus_corr.php">Seamus Corr</a><BR/>
-  <a href="contributors/alex_fox.php">Alex Fox</a><BR/>
-  <a href="contributors/Noah_George.php">Noah George</a> <BR/>
-  <a href="contributors/Nathan_Griffin.php">Nathan Griffin</a><BR/>
-  <a href="contributors/aidan_hanley.php">Aidan Hanley</a> <BR/>
-  <a href="contributors/Blake_Hildebrand.php">Blake Hildebrand</a><BR/>
-  <a href="contributors/Adam_johnson.php">Adam Johnson</a><BR/>
+  <a href="contributors/olivyah_vanek.php">Olivyah Vanek</a>, 
+  <a href="contributors/aidan_hanley.php">Aidan Hanley</a>,
+  <a href="contributors/joe_kutteh.php">Joseph Kutteh</a>, 
+  <a href="contributors/ethan_w.php">Ethan Weitkamp</a>, 
+  <a href="contributors/nick_nolt.php">Nicholas Nolt</a>, 
+  <a href="contributors/chris_voltz.php">Christopher Voltz</a>,
+  <a href="contributors/adam_omundsen.php">Adam Omundsen</a>, 
+  <a href="contributors/mansfield.php">Nathaniel Mansfield</a>, 
+  <a href="contributors/Freddie_Sanchez.php">Freddie Sanchez Govea</a>,
+  <a href="contributors/matthew_sutton.php">Matthew Sutton</a>, 
+  <a href="contributors/simon_burleson.php">Simon Burleson</a>, 
+  <a href="contributors/aiden_walmer.php">Aiden Walmer</a>,
+  <a href="contributors/alex_fox.php">Alex Fox</a>,  
+  <a href="contributors/Adam_johnson.php">Adam Johnson</a>,   
+  Derek Baldwin, 
+  <a href="contributors/august_bednar.php">August Bednar</a>, 
+  <a href="contributors/Blake_Hildebrand.php">Blake Hildebrand</a>, 
+  <a href="contributors/Nathan_Griffin.php">Nathan Griffin</a>,
+  <a href="contributors/Noah_George.php">Noah George</a>, 
+  <a href="contributors/seamus_corr.php">Seamus Corr</a>,
+  <a href="contributors/kelsey_knappenberger.php">Kelsey Knappenberger</a><BR/><BR/>
+Not Pictured:  <BR/>
 Gabriel Kimmel<BR/>
-<a href="contributors/kelsey_knappenberger.php">Kelsey Knappenberger</a><BR/>
-<a href="contributors/joe_kutteh.php">Joseph Kutteh</a><BR/>
-<a href="contributors/mansfield.php">Nathaniel Mansfield</a><BR/>
-<a href="contributors/nick_nolt.php">Nicholas Nolt</a><BR/>
-<a href="contributors/adam_omundsen.php">Adam Omundsen</a><BR/>
-<a href="contributors/Freddie_Sanchez.php">Freddie Sanchez Govea</a><BR/> 
-<a href="contributors/matthew_sutton.php">Matthew Sutton</a><BR/>
-<a href="contributors/olivyah_vanek.php">Olivyah Vanek</a><BR/>
-<a href="contributors/chris_voltz.php">Christopher Voltz</a><BR/>
-<a href="contributors/aiden_walmer.php">Aiden Walmer</a> <BR/>
-<a href="contributors/ethan_w.php">Ethan Weitkamp</a>  <BR/>
 <BR/>
+
 <a href="contributors/prof_reddig.php">Professor Nancy Reddig</a><BR/>
 
 

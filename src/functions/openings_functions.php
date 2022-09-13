2085695f5efc3d51a229b935f829dc17bd84729b
knappenbergerk <knappenbergerk@etown.edu>
2022-09-13 10:44:39 -0400

    Final merge for team_omega on REQ#5.2

--- src/functions/openings_functions.php
+++ src/functions/openings_functions.php
@@ -2,6 +2,7 @@
 function display_openings_form($openings=""){
 
 }
+/* Nav Bar */
 function display_openings_navigation($currentPage){
     $navHTML  = '<h4><div style="margin-top:5px;margin-bottom:45px;">';
     $navHTML .= '<a href="openings.php?page=search" class="selected">Search Jobs</a>';
@@ -10,14 +11,6 @@ function display_openings_navigation($currentPage){
 
     
     echo $navHTML;
-}
-function display_search_openings(){
-    echo '<h2>Search for an opening</h2><form method=get action="students.php">
-        Enter Opening Name:<input name="search" type="text">
-        <input name="page" type="hidden" value="search">
-        <input type="submit" value="Search">
-    </form><br/><br/>';
-
 }
 function get_job_by_name($word){
     if($word==""){
@@ -38,6 +31,7 @@ function get_all_openings_from_db(){
     $data = $pdo->query("SELECT * FROM job_opportunity order by title")->fetchAll();
     return $data;
 }
+/* Search Bar*/
 function display_search_form(){
     echo '<h2>Search For A Job Opening By Title:</h2><form method=get action="openings.php">
         Enter Job Opening: <input name="search" type="text">

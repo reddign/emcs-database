538e53f2bd6fc4341b8aaefc64dbd8ccdfd72883
gabekimmel <kimmelg@etown.edu>
2022-09-14 13:30:37 -0400

    change my name

--- src/contributors/gabe_kimmel.php
+++ src/contributors/gabe_kimmel.php
@@ -6,7 +6,7 @@ require("../includes/header.php");
  <!-- Header -->
   <div class="w3-container" style="margin-top:80px" id="showcase">
     <h1 class="w3-jumbo"><b>Contributor</b></h1>
-    <h1 class="w3-xxxlarge w3-text-red"><b>Gabriel Kimmel</b></h1>
+    <h1 class="w3-xxxlarge w3-text-red"><b>Gabe Kimmel</b></h1>
     <hr style="width:50px;border:5px solid red" class="w3-round">
   </div>
 

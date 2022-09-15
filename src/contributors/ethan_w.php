12a0d284e7cdcce52d3008368aa65ac762da5213
8ethan <weitkampe@etown.edu>
2022-09-15 14:19:00 -0400

    Test1

--- src/contributors/ethan_w.php
+++ src/contributors/ethan_w.php
@@ -10,6 +10,7 @@ require("../includes/header.php");
     <hr style="width:50px;border:5px solid red" class="w3-round">
   </div>
 
-  EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+  This is Ethan's page 6
+
 <?PHP
 require("../includes/footer.php");

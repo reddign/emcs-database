3baec2b112b0f1af3bbba993bf5d62066bd91c2d
NoahWGeorge <georgen1@etown.edu>
2022-09-16 22:22:27 -0400

    last commit

--- src/functions/meeting_form_functions.php
+++ src/functions/meeting_form_functions.php
@@ -199,4 +199,4 @@ function debug_to_console($data) {
 //POSSIBLE ADDS
 //clean up the look of the page
 //maybe add a calender and reminders for upcoming meetings?
-//This is the last commit for team Alpha, good luck on the final production!
+//This is the final commit from team aplha, good luck on the fina product to whatever team takes this over!
